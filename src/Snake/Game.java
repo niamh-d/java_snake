e5889708717f6172187659c5fdd8a1048da4940a
Niamh <niamh.linguist@gmail.com>
2025-04-05 13:14:17 +0300

    showing green square on screen

---
+++ src/Snake/Game.java
@@ -0,0 +1,41 @@
+package Snake;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    Tile snakeHead;
+
+    Game(int boardWidth, int boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.black);
+
+        snakeHead = new Tile(5,5);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+
+    }
+}
---
+++ src/Snake/Main.java
@@ -0,0 +1,21 @@
+package Snake;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Game game = new Game(boardWidth, boardHeight);
+        frame.add(game);
+        frame.pack();
+    }
+}

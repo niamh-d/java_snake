a5ef74dbd7a04c7b7b9c5955f6c5de27691be26c
Niamh <niamh.linguist@gmail.com>
2025-04-05 13:31:54 +0300

    food starts at random position

--- src/Snake/Game.java
+++ src/Snake/Game.java
@@ -2,6 +2,7 @@ package Snake;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Game extends JPanel {
     private class Tile {
@@ -19,6 +20,9 @@ public class Game extends JPanel {
 
     Tile snakeHead;
 
+    Tile food;
+    Random random;
+
     Game(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
@@ -26,6 +30,9 @@ public class Game extends JPanel {
         setBackground(Color.black);
 
         snakeHead = new Tile(5,5);
+        food = new Tile(10,10);
+        random = new Random();
+        placeFood();
     }
 
     public void paintComponent(Graphics g) {
@@ -34,8 +41,24 @@ public class Game extends JPanel {
     }
 
     public void draw(Graphics g) {
+        // grid
+        for(int i = 0; i < boardWidth/tileSize; i++) {
+            g.setColor(Color.darkGray);
+            g.drawLine(i * tileSize, 0, i * tileSize, boardHeight); // vertical lines
+            g.drawLine(0, i * tileSize, boardWidth, i * tileSize); // horizontal lines
+        }
+
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+        // snake
         g.setColor(Color.green);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
 
+    }
+
+    public void placeFood() {
+        food.x = random.nextInt(boardWidth/tileSize);
+        food.y = random.nextInt(boardHeight / tileSize);
     }
 }
